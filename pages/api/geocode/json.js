43a214276ce2d9de5e9595ec6c1a359fb22ecae7
Stefan Kühnel <git@stefankuehnel.com>
2020-12-30 22:22:20 +0100

    Exchange Geoapify with Nominatim.

--- pages/api/geocode/json.js
+++ pages/api/geocode/json.js
@@ -1,10 +1,10 @@
-import { sanitize } from '../../../utils/error/handler';
-import { lookup } from '../../../utils/geoapify';
+import { geoCodeHandler } from '../../../utils/handler/endpoints/geocode';
+import { errorHandler } from '../../../utils/handler/error';
 
 export default async function handler({ query: { address } }, res) {
   try {
-    res.status(200).json(await lookup(address));
+    res.status(200).json(await geoCodeHandler(address));
   } catch (error) {
-    res.status(error.statusCode).json(sanitize(error));
+    res.status(error.statusCode).json(errorHandler(error));
   }
 }
