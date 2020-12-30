b84319420dba3275626614e0664f239bf4c4af94
Stefan Kühnel <git@stefankuehnel.com>
2020-12-30 22:21:49 +0100

    Exchange Geoapify with Here API.

--- pages/api/directions/json.js
+++ pages/api/directions/json.js
@@ -1,10 +1,10 @@
-import { sanitize } from '../../../utils/error/handler';
-import { route } from '../../../utils/geoapify';
+import { routeHandler } from '../../../utils/handler/endpoints/route';
+import { errorHandler } from '../../../utils/handler/error';
 
 export default async function handler({ query: { origin, destination, mode, lang } }, res) {
   try {
-    res.status(200).json(await route(origin, destination, mode, lang));
+    res.status(200).json(await routeHandler(origin, destination, mode, lang));
   } catch (error) {
-    res.status(error.statusCode).json(sanitize(error));
+    res.status(error.statusCode).json(errorHandler(error));
   }
 }
